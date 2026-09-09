1ae412c3906f526cc8384c23f7e46aab78af2f35
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-09 05:53:53 +0100

    quotes are now replaces with new random quote, not just random - added api call attempt limit of 10 times for new quote

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -6,13 +6,21 @@ window.addEventListener("load", () => {
 });
 
 async function updateQuote() {
+  quoteContainer = document.getElementById("quote");
+  authorContainer = document.getElementById("author");
   try {
-    quote = await fetchQuote(QUOTE_API_URL);
-    document.getElementById("quote").innerText = quote.quote;
-    document.getElementById("author").innerText = quote.author;
+    let attempt = 0;
+    do {
+      attempt += 1;
+      new_quote = await fetchQuote(QUOTE_API_URL);
+    } while (quoteContainer.innerText == new_quote.quote && attempt < 10);
+
+    quoteContainer.innerText = new_quote.quote;
+    authorContainer.innerText = new_quote.author;
   } catch (error) {
     console.error(error);
   }
+  return new_quote;
 }
 
 async function fetchQuote(url) {
