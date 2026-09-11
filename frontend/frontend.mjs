4a86f087f9c3123871efcff53558ad88e4b7d866
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-12 00:15:58 +0100

    added post function for adding quotes using form

--- frontend/frontend.mjs
+++ frontend/frontend.mjs
@@ -1,8 +1,9 @@
-const QUOTE_API_URL = "https://wnwb6d7ykhntj8baadogfael.trainees.hosting.cyf.academy/";
+const QUOTE_BACKEND_URL = "https://wnwb6d7ykhntj8baadogfael.trainees.hosting.cyf.academy/";
 
 window.addEventListener("load", () => {
   updateQuote();
   document.getElementById("new-quote").addEventListener("click", updateQuote);
+  document.getElementById("add-quote").addEventListener("click", addQuote);
 });
 
 async function updateQuote() {
@@ -13,7 +14,7 @@ async function updateQuote() {
     let new_quote;
     do {
       attempt += 1;
-      new_quote = await fetchQuote(QUOTE_API_URL);
+      new_quote = await fetchQuote(QUOTE_BACKEND_URL);
     } while (quoteContainer.innerText == new_quote.quote && attempt < 10);
 
     quoteContainer.innerText = new_quote.quote;
@@ -21,12 +22,42 @@ async function updateQuote() {
   } catch (error) {
     console.error(error);
   }
-  return new_quote;
 }
 
 async function fetchQuote(url) {
-  return fetch(url).then((result) => {
-    if (!result.ok) throw new Error("Failed to load quote.");
-    return result.json();
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Failed to load quote.");
+  return response.json();
+}
+
+async function addQuote() {
+  const quoteInput = document.getElementById("quote-input");
+  const authorInput = document.getElementById("author-input");
+  const messageOutput = document.getElementById("output-message");
+
+  messageOutput.innerText = "";
+  if (quoteInput.value.trim() === "" || authorInput.value.trim() === "") {
+    messageOutput.innerText = "Please complete all fields.";
+    return;
+  }
+  const quote = { quote: quoteInput.value.trim(), author: authorInput.value.trim() };
+
+  try {
+    await postQuote(QUOTE_BACKEND_URL, quote);
+    quoteInput.value = "";
+    authorInput.value = "";
+    messageOutput.innerText = "Quote added successfully.";
+  } catch (error) {
+    console.error(error);
+    messageOutput.innerText = "Failed to add quote. Please try again.";
+  }
+}
+
+async function postQuote(url, quote) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(quote),
   });
+  if (!response.ok) throw new Error("Failed to add quote.");
 }
